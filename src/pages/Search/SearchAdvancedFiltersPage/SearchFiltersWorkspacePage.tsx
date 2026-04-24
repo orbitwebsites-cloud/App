09557153b551d52a30748812e38c4c247c6a066b
Situ Chandra Shil (via MelvinBot) <infra+melvinbot@expensify.com>
2026-04-24 18:07:24 +0000

    Fix: Replace deprecated absoluteFillObject with absoluteFill
    Co-authored-by: Situ Chandra Shil <situchan@users.noreply.github.com>

--- src/pages/Search/SearchAdvancedFiltersPage/SearchFiltersWorkspacePage.tsx
+++ src/pages/Search/SearchAdvancedFiltersPage/SearchFiltersWorkspacePage.tsx
@@ -110,7 +110,7 @@ function SearchFiltersWorkspacePage() {
                         }}
                     />
                     {shouldShowLoadingIndicator ? (
-                        <View style={[StyleSheet.absoluteFillObject, styles.fullScreenLoading, styles.w100, styles.flex1, styles.pRelative]}>
+                        <View style={[StyleSheet.absoluteFill, styles.fullScreenLoading, styles.w100, styles.flex1, styles.pRelative]}>
                             <View style={styles.w100}>
                                 <ActivityIndicator
                                     size={CONST.ACTIVITY_INDICATOR_SIZE.LARGE}
