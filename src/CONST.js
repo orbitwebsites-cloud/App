d63dc8daaf87fe3c34a38533b3d70d35e4c4509a
orbitwebsites-cloud <orbitwebsites@gmail.com>
2026-04-26 18:20:57 -0400

    fix: fix: [Due for payment 2026-04-20] [$250] Show Submit/Approve/Pay optio

---
+++ src/CONST.js
@@ -0,0 +1,20 @@
+const CONST = {
+    STYLES: {
+        BUTTON_WRAPPER: {
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            justifyContent: 'flex-start',
+            paddingHorizontal: 8,
+            paddingVertical: 8,
+            gap: 8,
+        },
+    },
+    SELECTION_LIST_ACTION: {
+        CLEAR: 'clear',
+        SUBMIT: 'submit',
+        APPROVE: 'approve',
+        PAY: 'pay',
+    },
+};
+
+export default CONST;
